f452f85d051afb670be218486639f7c9ae7de86d
Florian Putker <fputker@bol.com>
2020-04-24 14:25:17 +0200

    first styling

--- src/main/react/src/App.js
+++ src/main/react/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import SideMenu from "./components/sidemenu";
+import SideMenu from "./components/SideMenu";
+import TopMenu from "./components/TopMenu";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+}));
 
 const App = () => {
-    return (<SideMenu />)
+    const classes = useStyles();
+
+    return (<div className={classes.root}> <TopMenu /> <SideMenu />  <MainContent />  <Footer /> </div>)
+
 };
 
 export default App
