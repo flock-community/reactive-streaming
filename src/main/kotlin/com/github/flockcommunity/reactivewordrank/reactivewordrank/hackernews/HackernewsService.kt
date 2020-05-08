4d3faac514e2169c3cb70756f09d0e23787d6439
Florian Putker <fputker@bol.com>
2020-05-08 10:01:18 +0200

    first steps new endpoint

--- src/main/kotlin/com/github/flockcommunity/reactivewordrank/reactivewordrank/hackernews/HackernewsService.kt
+++ src/main/kotlin/com/github/flockcommunity/reactivewordrank/reactivewordrank/hackernews/HackernewsService.kt
@@ -1,7 +1,12 @@
 package com.github.flockcommunity.reactivewordrank.reactivewordrank.hackernews
 
+import org.slf4j.LoggerFactory
+
 class HackernewsService {
+    private val log = LoggerFactory.getLogger(javaClass)
 
     private val hn_url = "http://api.hnstream.com/news/stream/"
+
+    fun subscribe_to_hn()
 
 }
